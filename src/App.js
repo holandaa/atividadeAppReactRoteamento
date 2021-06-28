4125d69236eb740af53360af2362fdb75eb669f4
holandaa <holandawend@gmail.com>
2021-06-28 11:53:49 -0300

    App React com Roteamento

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+import ListaDeParticipantes from "./components/ListaDeParticipantes.js";
+import NovoParticipante from "./components/NovoParticipante.js";
+import EditaParticipante from "./components/EditaParticipante.js";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <nav className="container navbar navbar-expand navbar-dark bg-dark">
+        <h1 className="navbar-brand">
+          Exemplo CRUD
+        </h1>
+        <div className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <Link to={"/participantes"} className="nav-link">
+              Participantes
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to={"/novo"} className="nav-link">
+              Novo participante
+            </Link>
+          </li>
+        </div>
+      </nav>
+
+      <div className="container mt-3">
+        <Switch>
+          <Route exact path={["/", "/participantes"]} component={ListaDeParticipantes} />
+          <Route exact path="/novo" component={NovoParticipante} />
+          <Route path="/participantes/:id" component={EditaParticipante} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
